3833f08299fb9c3db117b44a6490e29859e35fb3
nishi240masa <masa2401106@gmail.com>
2024-12-26 12:52:38 +0900

    Refactor next.config.ts to enable export output

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // output: 'export',
+  output: 'export',
   // trailingSlash: true,
 };
-
-if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
-}
 
 export default nextConfig;
