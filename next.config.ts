3e0a31a94f2b738b731e449120762591d90b2684
nishi240masa <masa2401106@gmail.com>
2024-12-26 09:07:11 +0900

    add:wrangler

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   // output: 'export',
   // trailingSlash: true,
 };
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 export default nextConfig;
