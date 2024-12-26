e0790ce702657b0f99fba62a9a4025366e0b3797
nishi240masa <masa2401106@gmail.com>
2024-12-26 16:53:14 +0900

    Refactor build and preview scripts in package.json

--- src/app/(use-header)/production/page.tsx
+++ src/app/(use-header)/production/page.tsx
@@ -1,6 +1,8 @@
 // import TopPage from './_components/view/Page';
 import TopPage from './_components/view/Page';
 
+export const runtime = 'edge';
+
 export default function Page() {
   return <TopPage />;
 }
--- src/app/(use-header)/profyle/page.tsx
+++ src/app/(use-header)/profyle/page.tsx
@@ -1,5 +1,7 @@
 import TopPage from './_components/view/Page';
 
+export const runtime = 'edge';
+
 export default function Page() {
   return <TopPage />;
 }
