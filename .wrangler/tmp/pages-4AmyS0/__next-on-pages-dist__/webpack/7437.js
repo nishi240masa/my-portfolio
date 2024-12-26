67329138657b2ea348cac1a37d1c6aa8d5c47bc0
nishi240masa <masa2401106@gmail.com>
2024-12-26 12:29:35 +0900

    Refactor build configuration and dependencies

--- .wrangler/tmp/pages-4AmyS0/__next-on-pages-dist__/webpack/7437.js
+++
@@ -1,1 +0,0 @@
-var s={},u=(c,d,i)=>(s.__chunk_7437=(o,n,e)=>{"use strict";e.r(n),e.d(n,{default:()=>l});var t=e(4302),r=e(5960);function l({children:a}){return(0,t.jsxs)(t.Fragment,{children:[(0,t.jsx)(r.default,{}),(0,t.jsx)("main",{children:a})]})}},s);export{u as __getNamedExports};
