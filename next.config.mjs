1b5655a875dff89b68a5466e0ac5f4966060f04c
nishi240masa <masa2401106@gmail.com>
2025-01-20 16:03:41 +0900

    Update next.config.mjs and wrangler.toml

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone', 
+  experimental: {
+    appDir: true, 
+  },
+};
 
 if (process.env.NODE_ENV === 'development') {
   await setupDevPlatform();
